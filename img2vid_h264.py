77208a42ebf8de89e1087aa2be0af7a1e897d605
Ao Li <leoleexdu@gmail.com>
2024-01-15 16:32:25 +0800

    111

--- img2vid_h264.py
+++ img2vid_h264.py
@@ -2,14 +2,14 @@ import cv2
 import os
 
 # 图片文件夹路径
-image_folder = '000'  # 图片文件夹名
-video_name = 'output_video000.mp4'
+image_folder = '000_cv2_var100'  # 图片文件夹名
+video_name = 'output_videocv.mp4'
 
 # 视频的帧率
 fps = 30
 
 # 获取文件夹中的第一个图像文件，以确定视频的分辨率
-first_image = cv2.imread(os.path.join(image_folder, '00000000.png'))
+first_image = cv2.imread(os.path.join(image_folder, '00000001.png'))
 
 # 如果未能读取第一张图片，打印错误信息并退出
 if first_image is None:
@@ -22,7 +22,7 @@ fourcc = cv2.VideoWriter_fourcc(*'avc1')
 video = cv2.VideoWriter(video_name, fourcc, fps, (width, height))
 
 # 读取图片并添加到视频
-for i in range(100):
+for i in range(1,99):
     img_name = f"{i:08d}.png"  # 生成图片文件名
     img_path = os.path.join(image_folder, img_name)
     img = cv2.imread(img_path)
